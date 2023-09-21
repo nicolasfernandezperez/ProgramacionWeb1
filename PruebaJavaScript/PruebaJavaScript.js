da43bc946aba8c00ea108446c78780f92a6216d1
Nicolas Fernandez Perez <119294020+nicolasfernandezperez@users.noreply.github.com>
2023-09-21 12:37:17 +0200

    Update PruebaJavaScript.js

--- PruebaJavaScript/PruebaJavaScript.js
+++ PruebaJavaScript/PruebaJavaScript.js
@@ -1,16 +1,21 @@
 const operacion = +prompt("Introduce un tipo de operacion:\n1.Multiplicacion");
+
+const multiplicar = () =>{
+    const num1 = +prompt("Introduce el primer numero:");
+    const num2 = +prompt("Introduce el segundo numero:");
+    const resultado = num1 * num2;
+    alert(resultado);
+};
+
 switch(operacion){
     case 1: 
-        multiplicar()
+        multiplicar();
+    break;
+    default:
+        alert("Incorrecto");
     break;
 }
 
-const multiplicar=()=>{
-    const num1 = prompt("Introduce el primer numero:");
-    const num2 = prompt("Introduce el segundo numero:");
-    const resultado = num1 * num2;
-    alert(resultado);
-}
 
 
 
