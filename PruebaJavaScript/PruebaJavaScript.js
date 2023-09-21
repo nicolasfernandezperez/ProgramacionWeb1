2f4e7a0cb1b9a51a59bc7db0d6b19fd8b41746d9
Nicolas Fernandez Perez <119294020+nicolasfernandezperez@users.noreply.github.com>
2023-09-21 12:19:18 +0200

    Create PruebaJavaScript.js

---
+++ PruebaJavaScript/PruebaJavaScript.js
@@ -0,0 +1,13 @@
+const operacion = prompt("Introduce un tipo de operacion:");
+switch(operacion){
+    case 1: 
+        multiplicar()
+    break;
+}
+multiplicar =>{
+    prompt(num1);
+    prompt(num2);
+    alert(num1*num2);
+}
+
+
