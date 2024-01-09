ddb39f0b96fd1d5cf116b127f2fb633d24fcec1c
David Gil <davidgilvacas@gmail.com>
2024-01-09 21:27:19 +0100

    cxzcx
    ccxcz

--- practica-final-next/src/app/components/Admin/userList.jsx
+++ practica-final-next/src/app/components/Admin/userList.jsx
@@ -53,37 +53,34 @@ export default function UserList({ userFilter }) {
 
     return (
         <div className="max-w-4xl mx-auto my-8">
-            <h2 className="text-3xl text-gray-300 font-bold mb-4">Lista de Usuarios</h2>
-            {userFilter && userFilter.length > 0 ? (
-                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {userFilter.map((user, index) => (
-                        <li key={index} className="bg-white rounded-md overflow-hidden shadow-md">
-                            <img
-                                src={`https://picsum.photos/300/200?unique=${user.id}`}
-                                className="w-full h-40 object-cover object-center"
-                            />
-                            <div className="p-4  h-full ">
-                                <h4 className="text-sky-950 text-lg font-bold mb-2">{user.email}</h4>
-                                <p className="text-gray-900 font-bold">ID: <span className=" text-gray-500  ">{user.id}</span></p>
-                                <p className="text-gray-900 font-bold">Email: <span className=" text-gray-500  ">{user.email || 'No disponible'}</span></p>
-                                <p className="text-gray-900 font-bold">Fecha:<span className=" text-gray-500  "> {user.date || 'No disponible'}</span></p>
-                                <p className="text-gray-900 font-bold">Usuario: <span className=" text-gray-500  ">{user.userType || 'No disponible'}</span></p>
-                                <p className="text-gray-900 font-bold">Ciudad: <span className=" text-gray-500  ">{user.city || 'No disponible'}</span></p>
-                                <div className="mt-4 flex justify-end ">
-                                    <button
-                                        className="text-blue-500 hover:text-blue-700 font-medium focus:outline-none relative"
-                                        onClick={() => deleteUser(user.id)}
-                                    >
-                                        Eliminar usuario
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No hay usuarios disponibles</p>
-            )}
+          <h2 className="text-3xl text-black font-bold mb-4">Lista de Usuarios</h2>
+          {userFilter && userFilter.length > 0 ? (
+            <div className="grid grid-cols-1 gap-4">
+              {userFilter.map((user, index) => (
+                <div key={index} className="bg-white rounded-md overflow-hidden shadow-md w-full mb-4">
+                  <div className="p-4 h-full">
+                    <h4 className="text-sky-950 text-lg font-bold mb-2">{user.email}</h4>
+                    <p className="text-gray-900 font-bold">ID: <span className="text-gray-500">{user.id}</span></p>
+                    <p className="text-gray-900 font-bold">Email: <span className="text-gray-500">{user.email || 'No disponible'}</span></p>
+                    <p className="text-gray-900 font-bold">Fecha:<span className="text-gray-500">{user.date || 'No disponible'}</span></p>
+                    <p className="text-gray-900 font-bold">Usuario: <span className="text-gray-500">{user.userType || 'No disponible'}</span></p>
+                    <p className="text-gray-900 font-bold">Ciudad: <span className="text-gray-500">{user.city || 'No disponible'}</span></p>
+                    <div className="mt-4 flex justify-end">
+                      <button
+                        className="text-blue-500 hover:text-blue-700 font-medium focus:outline-none relative"
+                        onClick={() => deleteUser(user.id)}
+                      >
+                        Eliminar usuario
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>No hay usuarios disponibles</p>
+          )}
         </div>
-    );
+      );
+      
 }
