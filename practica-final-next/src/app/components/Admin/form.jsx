28cc98c3ac9b1079b3f0e10d1d15a32c76668cd0
Nicolas Fernandez Perez <119294020+nicolasfernandezperez@users.noreply.github.com>
2024-01-09 21:45:01 +0100

    Update users.txt

--- practica-final-next/src/app/components/Admin/form.jsx
+++ practica-final-next/src/app/components/Admin/form.jsx
@@ -47,122 +47,114 @@ function Form() {
     const { handleSubmit, reset, register, formState: { errors } } = useForm();
 
     return (
-        <div className="flex items-center justify-center flex-col ">
-            <div className='title text-center my-8'>
-                <h1 className="text-3xl font-bold text-gray-300 ">DAR DE ALTA UN COMERCIO</h1>
-                <p className="text-lg text-red-300 mt-2">Completa el formulario para registrar un nuevo comercio.</p>
+        <div className="flex items-center justify-center flex-col">
+          <div className="title text-center my-8">
+            <h1 className="text-3xl font-bold text-gray-800">DAR DE ALTA UN COMERCIO</h1>
+            <p className="text-lg text-red-500 mt-2">Completa el formulario para registrar un nuevo comercio.</p>
+          </div>
+      
+          <form className="w-full max-w-md rounded mt-8 adminForm" onSubmit={handleSubmit((data) => onSubmit(data, reset))}>
+      
+            <div className="mb-6">
+              <label className="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" htmlFor="commerceName">
+                Nombre del comercio
+                {errors.commerceName && (
+                  <span className="text-red-500 text-xs italic"> *</span>
+                )}
+              </label>
+              <input
+                {...register("commerceName", { required: true })}
+                className="appearance-none block w-full bg-white text-black border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
+                id="commerceName"
+                type="text"
+              />
             </div>
-
-            <form className="w-full max-w-lg rounded mt-8 adminForm" onSubmit={handleSubmit((data) => onSubmit(data, reset))}>
-
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="commerceName">
-                            Nombre del comercio
-                            {errors.commerceName && (
-                                <span className="text-red-500 text-xs italic"> *</span>
-                            )}
-                        </label>
-                        <input
-                            {...register("commerceName", { required: true })}
-                            className="appearance-none block w-full  text-black border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                            id="commerceName"
-                            type="text"
-                        />
-
-
-                    </div>
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="email">
-                            E-mail
-                            {errors.email && (
-                                <span className="text-red-500 text-xs italic"> *</span>
-                            )}
-                        </label>
-                        <input
-                            {...register("email", { required: true })}
-                            className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded mb-3 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="email"
-                            type="email"
-                        />
-
-
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-5">
-                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="phone">
-                            Telefono de contacto
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded mb-3 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            {...register('phone', {
-                                required: true,
-                                pattern: /^[0-9]{9}$/,
-                            })}
-                            type="tel"
-                            id="phone"
-                            maxLength="9"
-                        />
-                        {errors.phone && (
-                            <p className="text-red-500 text-xs italic">Formato incorrecto</p>
-                        )}
-
-                    </div>
-                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="cif">
-                            CIF
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded mb-3 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            {...register("cif", {
-                                required: true,
-                                pattern: /\d{8}[a-zA-Z]/,
-                            })}
-
-                            id="cif"
-                            type="text"
-                            maxLength="9"
-                        />
-                        <p></p>
-                        {errors.cif && (
-
-                            <p className="text-red-500 text-xs italic">Formato incorrecto</p>
-                        )}
-
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                        <label className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2" htmlFor="addres">
-                            Dirección
-                            {errors.addres && (
-                                <span className="text-red-500 text-xs italic"> *</span>
-                            )}
-                        </label>
-
-                        <input
-                            {...register("addres", { required: true })}
-                            className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="addres"
-                            type="search"
-                        />
-                        <p className="text-gray-400 text-xs italic">direccion del establecimiento</p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-end">
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit"
-                    >
-                        Añadir Comercio
-                    </button>
-                </div>
-            </form>
-
-            {Object.keys(errors).length > 0 && (
-                <p className="text-red-500 text-xs italic">Por favor, corrige los errores en el formulario.</p>
-            )}
+      
+            <div className="mb-6">
+              <label className="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" htmlFor="email">
+                E-mail
+                {errors.email && (
+                  <span className="text-red-500 text-xs italic"> *</span>
+                )}
+              </label>
+              <input
+                {...register("email", { required: true })}
+                className="appearance-none block w-full bg-white text-black border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
+                id="email"
+                type="email"
+              />
+            </div>
+      
+            <div className="mb-6">
+              <label className="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" htmlFor="phone">
+                Telefono de contacto
+              </label>
+              <input
+                className="appearance-none block w-full bg-white text-black border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
+                {...register('phone', {
+                  required: true,
+                  pattern: /^[0-9]{9}$/,
+                })}
+                type="tel"
+                id="phone"
+                maxLength="9"
+              />
+              {errors.phone && (
+                <p className="text-red-500 text-xs italic">Formato incorrecto</p>
+              )}
+            </div>
+      
+            <div className="mb-6">
+              <label className="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" htmlFor="cif">
+                CIF
+              </label>
+              <input
+                className="appearance-none block w-full bg-white text-black border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
+                {...register("cif", {
+                  required: true,
+                  pattern: /\d{8}[a-zA-Z]/,
+                })}
+                id="cif"
+                type="text"
+                maxLength="9"
+              />
+              {errors.cif && (
+                <p className="text-red-500 text-xs italic">Formato incorrecto</p>
+              )}
+            </div>
+      
+            <div className="mb-6">
+              <label className="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2" htmlFor="addres">
+                Dirección
+                {errors.addres && (
+                  <span className="text-red-500 text-xs italic"> *</span>
+                )}
+              </label>
+              <input
+                {...register("addres", { required: true })}
+                className="appearance-none block w-full bg-white text-black border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
+                id="addres"
+                type="search"
+              />
+              <p className="text-gray-500 text-xs italic">Dirección del establecimiento</p>
+            </div>
+      
+            <div className="flex items-center justify-end">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Añadir Comercio
+              </button>
+            </div>
+          </form>
+      
+          {Object.keys(errors).length > 0 && (
+            <p className="text-red-500 text-xs italic">Por favor, corrige los errores en el formulario.</p>
+          )}
         </div>
-    );
+      );
+      
 }
 
 export default Form;
--- practica-final-next/src/app/components/Commerce/commerce.jsx
+++ practica-final-next/src/app/components/Commerce/commerce.jsx
@@ -1,43 +1,86 @@
 "use client";
+import toast from 'react-hot-toast';
+const deleteComerces = async (commerceID) => {
+    const confirmDelete = window.confirm("¿Estás seguro de querer eliminar este comercio?");
+    console.log("Borrar:", commerceID)
+    if (!confirmDelete) {
+        toast("Operacion cancelada")
+        return;
+    } else {
+        try {
+            const response = await fetch('/api/commerce', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(commerceID),
+            });
 
+            if (!response.ok) {
+                toast.error("Error al eliminar el usuario")
+                console.error('Error al eliminar el comercio. Código de estado:', response.status);
+                return;// Puedes manejar el error de alguna manera, mostrar un mensaje, etc.
+            } else {
+
+                toast.loading("Eliminando...", { duration: 2000 })
+
+                setTimeout(() => {
+
+                    toast.success("comercio eliminado")
+                }, 2000)
+
+
+                const result = await response.json();
+                console.log(result.message);
+                // Puedes mostrar un mensaje de éxito o realizar alguna acción adicional.
+            }
+        } catch (error) {
+            toast.error("Ha ocurrido un error inesperado")
+            console.error('Error en la función DELETE:', error);
+
+        } finally {
+            setTimeout(() => {
+                window.location.reload();
+            }, 3000)
+        }
+    }
+}
 export default function Commerces( {commerces} ) {
     // El array vacío asegura que el efecto solo se ejecute una vez al montar el componente.
 
     return (
         <div className="max-w-4xl mx-auto my-8">
-            <h2 className="text-3xl text-gray-300 font-bold mb-4">Lista de Comercios</h2>
-            {commerces && commerces.length > 0 ? (
-                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {commerces.map((commerce, index) => (
-                        <li key={index} className="bg-white rounded-md overflow-hidden shadow-md">
-                            <img
-                                src={`https://picsum.photos/300/200?unique=${commerce.id}`}
-                                alt={commerce.commerceName}
-                                className="w-full h-40 object-cover object-center"
-                            />
-                            <div className="p-4  h-full ">
-                                <h4 className="text-sky-950 text-lg font-bold mb-2">{commerce.commerceName}</h4>
-                                <p className="text-gray-900 font-bold">ID: <span className=" text-gray-500  ">{commerce.id}</span></p>
-                                <p className="text-gray-900 font-bold">Email: <span className=" text-gray-500  ">{commerce.email || 'No disponible'}</span></p>
-                                <p className="text-gray-900 font-bold">Teléfono:<span className=" text-gray-500  "> {commerce.phone || 'No disponible'}</span></p>
-                                <p className="text-gray-900 font-bold">CIF: <span className=" text-gray-500  ">{commerce.cif || 'No disponible'}</span></p>
-                                <p className="text-gray-900 font-bold">Dirección: <span className=" text-gray-500  ">{commerce.addres || 'No disponible'}</span></p>
-                                <div className="mt-4 flex justify-end ">
-                                    <button
-                                        className="text-blue-500 hover:text-blue-700 font-medium focus:outline-none relative"
-                                        onClick={() => deleteComerces(commerce.id)}
-                                    >
-                                        Eliminar comercio
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p className='text-white'>No hay comercios disponibles</p>
-            )}
+          <h2 className="text-3xl text-black font-bold mb-4">Lista de Comercios</h2>
+          {commerces && commerces.length > 0 ? (
+            <div className="grid grid-cols-1 gap-4">
+              {commerces.map((commerce, index) => (
+                <div key={index} className="bg-white rounded-md overflow-hidden shadow-md w-full mb-4">
+                  <div className="p-4">
+                    <h4 className="text-sky-950 text-lg font-bold mb-2">{commerce.commerceName}</h4>
+                    <p className="text-gray-900 font-bold">ID: <span className="text-gray-500">{commerce.id}</span></p>
+                    <p className="text-gray-900 font-bold">Email: <span className="text-gray-500">{commerce.email || 'No disponible'}</span></p>
+                    <p className="text-gray-900 font-bold">Teléfono:<span className="text-gray-500">{commerce.phone || 'No disponible'}</span></p>
+                    <p className="text-gray-900 font-bold">CIF: <span className="text-gray-500">{commerce.cif || 'No disponible'}</span></p>
+                    <p className="text-gray-900 font-bold">Dirección: <span className="text-gray-500">{commerce.addres || 'No disponible'}</span></p>
+                    <div className="mt-4 flex justify-end">
+            <button
+                                className="text-blue-500 hover:text-blue-700 font-medium focus:outline-none relative"
+                                onClick={() => deleteComerces(commerce.id)}
+                        >
+                Eliminar comercio
+                            </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p className='text-white'>No hay comercios disponibles</p>
+          )}
         </div>
-    );
+      );
+      
+      
+      
 
 }
